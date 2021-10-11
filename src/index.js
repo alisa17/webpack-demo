b079bdb64da78c5c8f003e5490a8cc65ec4433ed
alisa <alisayunusova@Alisas-MacBook-Pro.local>
2021-10-12 11:24:16 +1300

    add css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
+import './style.css';
+
 function component() {
     const element = document.createElement('div');
   
     element.innerHTML = 'Hello webpack!!';
+    element.classList.add('hello');
   
     return element;
   }
